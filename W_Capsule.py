fea8547380393789249523a6bde5caa0c8b125a0
Skii <56576955+Kickskii@users.noreply.github.com>
2026-03-11 06:34:01 +0530

    Refactor WCapsule code to work with blender 5.0

--- W_Capsule.py
+++ W_Capsule.py
@@ -1,116 +1,131 @@
 # __________________________________/
 # __Author:_________Vit_Prochazka___/
-# __Created:________16.07.2019______/
-# __Version:________1.0_____________/
+# __Change Author:__Skii____________/
+# __Created:________03.04.2018______/
+# __Last_modified:__10.03.2026______/
+# __Version:_0.1_Sub1(Community_Fix)/
 # __________________________________/
 
-# imports
 import bpy
-from bpy.props import (
-    FloatProperty,
-    IntProperty,
-    BoolProperty,
-    PointerProperty
-)
 from mathutils import Vector, Quaternion
 from math import pi as PI
-from .genFunctions import (
+from bpy.props import BoolProperty, IntProperty, FloatProperty, PointerProperty
+
+from .gen_func import (
     circleVerts as circ_V,
     moveVerts as move_V,
     fanClose,
     bridgeLoops,
-    create_mesh_object
+    create_mesh_object as c_mesh
 )
 
-# generate geometry
-def geoGen_WCapsule (
-    radius,
-    height,
-    seg_perimeter,
-    seg_height,
-    seg_caps,
-    centered,
-    smoothed):
-    
-    # Prepare empty lists
+
+WCapsule_Defaults = {
+    "radius": 0.5,
+    "height": 2.0,
+    "seg_perimeter": 24,
+    "seg_height": 1,
+    "seg_caps": 8,
+    "centered": True,
+    "smoothed": True
+}
+
+
+def primitive_Capsule_ME(
+        radius=0.5,
+        height=2.0,
+        seg_perimeter=24,
+        seg_height=1,
+        seg_caps=8,
+        centered=True,
+        smoothed=True):
+
     verts = []
     edges = []
     faces = []
-
     loops = []
 
-    # Set minimums
     if seg_perimeter < 3:
         seg_perimeter = 3
+
     if seg_height < 1:
         seg_height = 1
+
     if seg_caps < 1:
         seg_caps = 1
+
     if radius > height / 2:
         radius = height / 2
 
-    # Add top and bottom center vertices
     verts.append(Vector((0, 0, 0)))
     verts.append(Vector((0, 0, height)))
 
-    # Create bootom cap segmentation loops
     if seg_caps > 1:
         angleStep = PI / (2 * seg_caps)
+
         for i in range(1, seg_caps):
-            # find the radius and height
+
             quat = Quaternion((0, -1, 0), i * angleStep)
             helperVect = quat @ Vector((0, 0, -radius))
+
             segmentRadius = helperVect.x
             segmentHeight = radius + helperVect.z
-            # create the ring
+
             newVerts, loop = circ_V(segmentRadius, seg_perimeter, len(verts))
             move_V(newVerts, Vector((0, 0, segmentHeight)))
+
             verts.extend(newVerts)
             loops.append(loop)
 
-    # Create the base corner circle
     newVerts, loop = circ_V(radius, seg_perimeter, len(verts))
     move_V(newVerts, Vector((0, 0, radius)))
+
     verts.extend(newVerts)
     loops.append(loop)
 
-    # Create the side segmentation loops
     if height > 2 * radius:
+
         if seg_height > 1:
+
             heightStep = (height - (2 * radius)) / seg_height
+
             for i in range(1, seg_height):
+
                 newHeight = (i * heightStep) + radius
+
                 newVerts, loop = circ_V(radius, seg_perimeter, len(verts))
                 move_V(newVerts, Vector((0, 0, newHeight)))
+
                 verts.extend(newVerts)
                 loops.append(loop)
 
-    # Create top corner circle
         newVerts, loop = circ_V(radius, seg_perimeter, len(verts))
         move_V(newVerts, Vector((0, 0, height - radius)))
+
         verts.extend(newVerts)
         loops.append(loop)
 
-    # Create top cap segmentation loops
     if seg_caps > 1:
+
         angleStep = PI / (2 * seg_caps)
+
         for i in range(1, seg_caps):
-            # find the radius and height
+
             quat = Quaternion((0, -1, 0), i * angleStep)
             helperVect = quat @ Vector((radius, 0, 0))
+
             segmentRadius = helperVect.x
             segmentHeight = height - radius + helperVect.z
-            # create the ring
+
             newVerts, loop = circ_V(segmentRadius, seg_perimeter, len(verts))
             move_V(newVerts, Vector((0, 0, segmentHeight)))
+
             verts.extend(newVerts)
             loops.append(loop)
 
-    # Close caps
-    faces.extend(fanClose(loops[0], 0, flipped = True))
+    faces.extend(fanClose(loops[0], 0, flipped=True))
     faces.extend(fanClose(loops[-1], 1))
 
-    # Bridge all loops
     for i in range(1, len(loops)):
         faces.extend(bridgeLoops(loops[i - 1], loops[i], True))
 
@@ -119,139 +134,138 @@ def geoGen_WCapsule (
 
     return verts, edges, faces
 
-def update_WCapsule (wData):
-    return geoGen_WCapsule (
-        radius = wData.rad_1,
-        height = wData.siz_z,
-        seg_perimeter = wData.seg_1,
-        seg_height = wData.seg_2,
-        seg_caps = wData.seg_3,
-        centered = wData.cent,
-        smoothed = wData.smo
+
+def update_capsule(self, context):
+
+    if context.object is None:
+        return
+
+    mesh = context.object.data
+
+    verts, edges, faces = primitive_Capsule_ME(
+        self.radius,
+        self.height,
+        self.seg_perimeter,
+        self.seg_height,
+        self.seg_caps,
+        self.centered,
+        self.smoothed
     )
 
-# add object W_Plane
-class Make_WCapsule(bpy.types.Operator):
-    """Create primitive wCapsule"""
-    bl_idname = "mesh.make_wcapsule"
-    bl_label = "wCapsule"
-    bl_options = {'UNDO', 'REGISTER'}
+    mesh.clear_geometry()
+    mesh.from_pydata(verts, edges, faces)
+    mesh.update()
+
+    for poly in mesh.polygons:
+        poly.use_smooth = self.smoothed
+
+
+class WCapsuleData(bpy.types.PropertyGroup):
 
     radius: FloatProperty(
-        name = "Radius",
-        description = "Radius",
-        default = 0.5,
-        min = 0.0,
-        soft_min = 0.0,
-        step = 1,
-        unit = "LENGTH"
+        name="Radius",
+        default=0.5,
+        min=0.0,
+        update=update_capsule
     )
 
     height: FloatProperty(
-        name = "Height",
-        description = "Height of the capsule",
-        default = 2.0,
-        min = 0.0,
-        soft_min = 0.0,
-        step = 1,
-        unit = "LENGTH"
+        name="Height",
+        default=2.0,
+        min=0.0,
+        update=update_capsule
     )
 
     seg_perimeter: IntProperty(
-        name = "Perim Segments",
-        description = "Subdivision on perimeter",
-        default = 24,
-        min = 3,
-        soft_min = 3,
-        step = 1,
-        subtype = 'NONE'
+        name="Perimeter",
+        default=24,
+        min=3,
+        update=update_capsule
     )
 
     seg_height: IntProperty(
-        name = "Height Segments",
-        description = "Subdivision of the height",
-        default = 1,
-        min = 1,
-        soft_min = 1,
-        step = 1,
-        subtype = 'NONE'
+        name="Height Segments",
+        default=1,
+        min=1,
+        update=update_capsule
     )
 
     seg_caps: IntProperty(
-        name = "Caps Segments",
-        description = "Subdivision of the caps",
-        default = 6,
-        min = 1,
-        soft_min = 1,
-        step = 1,
-        subtype = 'NONE'
+        name="Caps Segments",
+        default=6,
+        min=1,
+        update=update_capsule
     )
 
     centered: BoolProperty(
-        name = "Centered",
-        description = "Set origin of the cone",
-        default = False
+        name="Centered",
+        default=False,
+        update=update_capsule
     )
 
     smoothed: BoolProperty(
-        name = "Smooth",
-        description = "Set smooth shading",
-        default = True
+        name="Smooth",
+        default=True,
+        update=update_capsule
     )
+
+
+class Make_WCapsule(bpy.types.Operator):
+
+    bl_idname = "mesh.make_wcapsule"
+    bl_label = "WCapsule"
+    bl_options = {'UNDO', 'REGISTER'}
 
     def execute(self, context):
 
-        verts, edges, faces = geoGen_WCapsule(
-            radius = self.radius,
-            height = self.height,
-            seg_perimeter = self.seg_perimeter,
-            seg_height = self.seg_height,
-            seg_caps = self.seg_caps,
-            centered = self.centered,
-            smoothed = self.smoothed
-        )
-        create_mesh_object(context, verts, edges, faces, "wCapsule")
+        verts, edges, faces = primitive_Capsule_ME(**WCapsule_Defaults)
 
-        wD = context.object.data.wData
-        wD.rad_1 = self.radius
-        wD.siz_z = self.height
-        wD.seg_1 = self.seg_perimeter
-        wD.seg_2 = self.seg_height
-        wD.seg_3 = self.seg_caps
-        wD.cent = self.centered
-        wD.smo = self.smoothed
-        wD.wType = 'WCAPSULE'
+        obj = c_mesh(context, verts, edges, faces, "WCapsule")
+        mesh = obj.data
 
-        bpy.ops.object.shade_smooth()
-        context.object.data.use_auto_smooth = True
-        context.object.data.auto_smooth_angle = 1.0
+        mesh.WType = 'WCAPSULE'
+
+        mesh.WCapsule.radius = WCapsule_Defaults["radius"]
+        mesh.WCapsule.height = WCapsule_Defaults["height"]
+        mesh.WCapsule.seg_perimeter = WCapsule_Defaults["seg_perimeter"]
+        mesh.WCapsule.seg_height = WCapsule_Defaults["seg_height"]
+        mesh.WCapsule.seg_caps = WCapsule_Defaults["seg_caps"]
+        mesh.WCapsule.centered = WCapsule_Defaults["centered"]
+        mesh.WCapsule.smoothed = WCapsule_Defaults["smoothed"]
+
         return {'FINISHED'}
 
-# create UI panel
-def draw_WCapsule_panel(self, context):
-    lay_out = self.layout
-    lay_out.use_property_split = True
-    WData = context.object.data.wData
 
-    lay_out.label(text="Type: wCapsule", icon='MESH_CAPSULE')
+def drawWCapsulePanel(self, context):
 
-    col = lay_out.column(align=True)
-    col.prop(WData, "rad_1", text="Radius")
-    col.prop(WData, "siz_z", text="Height")
-    
-    col = lay_out.column(align=True)
-    col.prop(WData, "seg_1", text="Segmentation Main")
-    col.prop(WData, "seg_2", text="Vertical")
-    col.prop(WData, "seg_3", text="Caps")
+    layout = self.layout
+    data = context.object.data.WCapsule
 
-    lay_out.prop(WData, "cent", text="Centered")
-    lay_out.prop(WData, "smo", text="Smooth Shading")
-    lay_out.prop(WData, "anim", text="Animated")
+    layout.label(text="Type: WCapsule", icon="MESH_CAPSULE")
+
+    row = layout.row()
+
+    col = row.column(align=True)
+    col.prop(data, "radius")
+    col.prop(data, "height")
+
+    col = row.column(align=True)
+    col.prop(data, "seg_perimeter")
+    col.prop(data, "seg_height")
+    col.prop(data, "seg_caps")
+
+    row = layout.row(align=True)
+    row.prop(data, "centered")
+    row.prop(data, "smoothed")
 
 
-# register
-def reg_wCapsule():
+def registerWCapsule():
     bpy.utils.register_class(Make_WCapsule)
-# unregister
-def unreg_wCapsule():
+    bpy.utils.register_class(WCapsuleData)
+    bpy.types.Mesh.WCapsule = PointerProperty(type=WCapsuleData)
+
+
+def unregisterWCapsule():
     bpy.utils.unregister_class(Make_WCapsule)
+    bpy.utils.unregister_class(WCapsuleData)
+    del bpy.types.Mesh.WCapsule
