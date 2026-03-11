6baa70015724992bad6c78409afa78971e854eed
Skii <56576955+Kickskii@users.noreply.github.com>
2026-03-11 06:32:16 +0530

    Refactor W_Box.py for to work with blender 5.0

--- W_Box.py
+++ W_Box.py
@@ -1,26 +1,40 @@
 # __________________________________/
 # __Author:_________Vit_Prochazka___/
-# __Created:________16.07.2019______/
-# __Version:________1.0_____________/
+# __Change Author:__Skii/
+# __Created:________15.12.2015______/
+# __Last_modified:__10.03.2026______/
+# __Version:_0.3_Sub1(Community_Fix)/
 # __________________________________/
 
-# imports
-import bpy
-from bpy.props import (
-    FloatProperty,
-    IntProperty,
-    BoolProperty,
-    PointerProperty
-)
-from mathutils import Vector
-from .genFunctions import bridgeLoops, create_mesh_object
+"""
+Modernized WBox primitive for Blender 5
+"""
 
-# generate geometry
-def geoGen_WBox (size_x, size_y, size_z, seg_x, seg_y, seg_z, centered):
-    
-    if seg_x < 1: seg_x = 1
-    if seg_y < 1: seg_y = 1
-    if seg_z < 1: seg_z = 1
+import bpy
+from bpy.props import FloatProperty, IntProperty, BoolProperty, PointerProperty
+from mathutils import Vector
+from .gen_func import bridgeLoops, create_mesh_object
+
+
+WBox_Defaults = {
+    "size_x": 2.0,
+    "size_y": 2.0,
+    "size_z": 2.0,
+    "seg_x": 1,
+    "seg_y": 1,
+    "seg_z": 1,
+    "centered": True
+}
+
+
+def primitive_Box(
+        size_x=2.0,
+        size_y=2.0,
+        size_z=2.0,
+        seg_x=1,
+        seg_y=1,
+        seg_z=1,
+        centered=True):
 
     verts = []
     edges = []
@@ -55,9 +69,9 @@ def geoGen_WBox (size_x, size_y, size_z, seg_x, seg_y, seg_z, centered):
     for i in range(seg_x + 1):
         loop.append(bottom_lines[0][i])
     for i in range(seg_y - 1):
-        loop.append(bottom_lines[i + 1][- 1])
+        loop.append(bottom_lines[i + 1][-1])
     for i in range(seg_x + 1):
-        loop.append(bottom_lines[- 1][-(i + 1)])
+        loop.append(bottom_lines[-1][-(i + 1)])
     for i in range(seg_y - 1):
         loop.append(bottom_lines[-(i + 2)][0])
     loops.append(loop)
@@ -73,12 +87,10 @@ def geoGen_WBox (size_x, size_y, size_z, seg_x, seg_y, seg_z, centered):
             verts.append(Vector((size_x, (i + 1) * dist_y, (z + 1) * dist_z)))
         for i in range(seg_x + 1):
             loop.append(len(verts))
-            verts.append(Vector((
-                size_x - (i * dist_x), size_y, (z + 1) * dist_z)))
+            verts.append(Vector((size_x - (i * dist_x), size_y, (z + 1) * dist_z)))
         for i in range(seg_y - 1):
             loop.append(len(verts))
-            verts.append(Vector((
-                0.0, size_y - ((i + 1) * dist_y), (z + 1) * dist_z)))
+            verts.append(Vector((0.0, size_y - ((i + 1) * dist_y), (z + 1) * dist_z)))
         loops.append(loop)
 
     # top loop
@@ -93,15 +105,15 @@ def geoGen_WBox (size_x, size_y, size_z, seg_x, seg_y, seg_z, centered):
         loop.append(top_lines[-(i + 2)][0])
     loops.append(loop)
 
-    # faces bottom
+    # bottom faces
     for i in range(seg_y):
-        faces.extend(bridgeLoops(bottom_lines[i+1], bottom_lines[i], False))
+        faces.extend(bridgeLoops(bottom_lines[i], bottom_lines[i + 1], False))
 
-    # faces top
+    # top faces
     for i in range(seg_y):
         faces.extend(bridgeLoops(top_lines[i], top_lines[i + 1], False))
 
-    # faces sides
+    # side faces
     for i in range(seg_z):
         faces.extend(bridgeLoops(loops[i], loops[i + 1], True))
 
@@ -109,140 +121,104 @@ def geoGen_WBox (size_x, size_y, size_z, seg_x, seg_y, seg_z, centered):
         half_x = size_x / 2
         half_y = size_y / 2
         half_z = size_z / 2
-        for vertex in verts:
-            vertex -= Vector((half_x, half_y, half_z))
+        for v in verts:
+            v -= Vector((half_x, half_y, half_z))
 
     return verts, edges, faces
 
-def update_WBox (wData):
-    return geoGen_WBox (
-        size_x=wData.siz_x,
-        size_y=wData.siz_y,
-        size_z=wData.siz_z,
-        seg_x=wData.seg_1,
-        seg_y=wData.seg_2,
-        seg_z=wData.seg_3,
-        centered=wData.cent
+
+def update_box(self, context):
+
+    if context.object is None:
+        return
+
+    mesh = context.object.data
+
+    verts, edges, faces = primitive_Box(
+        self.size_x,
+        self.size_y,
+        self.size_z,
+        self.seg_x,
+        self.seg_y,
+        self.seg_z,
+        self.centered
     )
 
-# add object W_Plane
+    mesh.clear_geometry()
+    mesh.from_pydata(verts, edges, faces)
+    mesh.update()
+
+
+class WBoxData(bpy.types.PropertyGroup):
+
+    size_x: FloatProperty(name="X", default=2.0, min=0.0, update=update_box)
+    size_y: FloatProperty(name="Y", default=2.0, min=0.0, update=update_box)
+    size_z: FloatProperty(name="Z", default=2.0, min=0.0, update=update_box)
+
+    seg_x: IntProperty(name="X", default=1, min=1, update=update_box)
+    seg_y: IntProperty(name="Y", default=1, min=1, update=update_box)
+    seg_z: IntProperty(name="Z", default=1, min=1, update=update_box)
+
+    centered: BoolProperty(name="Centered", default=True, update=update_box)
+
+
 class Make_WBox(bpy.types.Operator):
-    """Create primitive wBox"""
+    """Create primitive WBox"""
     bl_idname = "mesh.make_wbox"
-    bl_label = "wBox"
+    bl_label = "WBox"
     bl_options = {'UNDO', 'REGISTER'}
-
-    size_x: FloatProperty(
-        name = "Width (X)",
-        description = "Size of the WBox",
-        default = 2.0,
-        min = 0.0,
-        soft_min = 0.0,
-        step = 1,
-        unit='LENGTH'
-    )
-
-    size_y: FloatProperty(
-        name = "Length (Y)",
-        description = "Size of the WBox",
-        default = 2.0,
-        min = 0.0,
-        soft_min = 0.0,
-        step = 1,
-        unit='LENGTH'
-    )
-
-    size_z: FloatProperty(
-        name = "Height (Z)",
-        description = "Size of the WBox",
-        default = 2.0,
-        min = 0.0,
-        soft_min = 0.0,
-        step = 1,
-        unit='LENGTH'
-    )
-
-    seg_x: IntProperty(
-        name = "Segments X",
-        description = "Segmentation of the WBox",
-        default = 1,
-        min = 1,
-        soft_min = 1,
-        step = 1
-    )
-
-    seg_y: IntProperty(
-        name = "Segments Y",
-        description = "Segmentation of the WBox",
-        default = 1,
-        min = 1,
-        soft_min = 1,
-        step = 1
-    )
-
-    seg_z: IntProperty(
-        name = "Segments Z",
-        description = "Segmentation of the WBox",
-        default = 1,
-        min = 1,
-        soft_min = 1,
-        step = 1
-    )
-
-    centered: BoolProperty(
-        name = "Centered",
-        description = "Where is origin of the WBox",
-        default = True
-    )
 
     def execute(self, context):
 
-        verts, edges, faces = geoGen_WBox(
-            size_x=self.size_x,
-            size_y=self.size_y,
-            size_z=self.size_z,
-            seg_x=self.seg_x,
-            seg_y=self.seg_y,
-            seg_z=self.seg_z,
-            centered=self.centered
-        )
-        create_mesh_object(context, verts, edges, faces, "wBox")
+        verts, edges, faces = primitive_Box(**WBox_Defaults)
 
-        wD = context.object.data.wData
-        wD.siz_x = self.size_x
-        wD.siz_y = self.size_y
-        wD.siz_z = self.size_z
-        wD.seg_1 = self.seg_x
-        wD.seg_2 = self.seg_y
-        wD.seg_3 = self.seg_z
-        wD.cent = self.centered
-        wD.wType = 'WBOX'
+        obj = create_mesh_object(context, verts, edges, faces, "WBox")
+        mesh = obj.data
+
+        mesh.WType = 'WBOX'
+
+        mesh.WBox.size_x = WBox_Defaults["size_x"]
+        mesh.WBox.size_y = WBox_Defaults["size_y"]
+        mesh.WBox.size_z = WBox_Defaults["size_z"]
+        mesh.WBox.seg_x = WBox_Defaults["seg_x"]
+        mesh.WBox.seg_y = WBox_Defaults["seg_y"]
+        mesh.WBox.seg_z = WBox_Defaults["seg_z"]
+        mesh.WBox.centered = WBox_Defaults["centered"]
+
         return {'FINISHED'}
-# create UI panel
-def draw_WBox_panel(self, context):
-    lay_out = self.layout
-    lay_out.use_property_split = True
-    WData = context.object.data.wData
-
-    lay_out.label(text="Type: wBox", icon='MESH_CUBE')
-
-    col = lay_out.column(align=True)
-    col.prop(WData, "siz_x", text="Size X")
-    col.prop(WData, "siz_y", text="Y")
-    col.prop(WData, "siz_z", text="Z")
-    
-    col = lay_out.column(align=True)
-    col.prop(WData, "seg_1", text="Segmentation X")
-    col.prop(WData, "seg_2", text="Y")
-    col.prop(WData, "seg_3", text="Z")
-
-    lay_out.prop(WData, "cent", text="Centered")
-    lay_out.prop(WData, "anim", text="Animated")
 
 
-# register
-def reg_wBox():
+def drawWBoxPanel(self, context):
+
+    layout = self.layout
+    data = context.object.data.WBox
+
+    layout.label(text="Type: WBox", icon='MESH_CUBE')
+
+    row = layout.row()
+
+    col = row.column(align=True)
+    col.label(text="Size")
+    col.prop(data, "size_x")
+    col.prop(data, "size_y")
+    col.prop(data, "size_z")
+
+    col = row.column(align=True)
+    col.label(text="Segments")
+    col.prop(data, "seg_x")
+    col.prop(data, "seg_y")
+    col.prop(data, "seg_z")
+
+    layout.prop(data, "centered")
+
+
+def registerWBox():
     bpy.utils.register_class(Make_WBox)
-# unregister
-def unreg_wBox():
+    bpy.utils.register_class(WBoxData)
+    bpy.types.Mesh.WBox = PointerProperty(type=WBoxData)
+
+
+def unregisterWBox():
     bpy.utils.unregister_class(Make_WBox)
+    bpy.utils.unregister_class(WBoxData)
+    del bpy.types.Mesh.WBox
